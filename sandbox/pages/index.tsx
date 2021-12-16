01d677f762f8fd8cafa8e80451fe654412b9b5cf
RIOU Kevin <kevin@riou.pro>
2021-12-16 17:29:41 +0100

    ✨feat (Meta): add meta data for socials

--- sandbox/pages/index.tsx
+++ sandbox/pages/index.tsx
@@ -14,15 +14,34 @@ import VariableInput from '../components/VariablesInput';
 import VariablesRenderer from '../components/VariablesRenderer';
 import styles from '../styles/Home.module.css';
 
+const TITLE = 'Configurator - VSCode Git commit message';
+const DESCRIPTION =
+  'A settings generator for the extension Git commit message of VSCode';
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Config - Git commit message</title>
-        <meta
-          name="description"
-          content="Create your config for vscode git commit message"
-        />
+        <meta charSet="utf-8" />
+        <title>{TITLE}</title>
+        <meta name="description" content={DESCRIPTION} />
+        <meta name="image" content="/logo.png" />
+        <meta itemProp="name" content={TITLE} />
+        <meta itemProp="description" content={DESCRIPTION} />
+        <meta itemProp="image" content="/logo.png" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={TITLE} />
+        <meta name="twitter:description" content={DESCRIPTION} />
+        <meta name="twitter:site" content="@RiouKevin6" />
+        <meta name="twitter:creator" content="@RiouKevin6" />
+        <meta name="twitter:image:src" content="/logo.png" />
+        <meta name="og:title" content={TITLE} />
+        <meta name="og:description" content={DESCRIPTION} />
+        <meta name="og:image" content="/logo.png" />
+        <meta name="og:url" content="https://gcm-config.netlify.com" />
+        <meta name="og:site_name" content="Config - Git commit message" />
+        <meta name="og:locale" content="en_US" />
+        <meta name="og:type" content="website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
